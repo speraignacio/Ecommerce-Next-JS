f6fea62ae75b04ac7fb6edd0ddd74d99c7b60fe6
speraignacio <speraignacio@gmail.com>
2021-08-14 12:38:59 +0200

    install form yup & formik

--- components/Auth/RegisterForm/RegisterForm.js
+++ components/Auth/RegisterForm/RegisterForm.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Form, Button } from "semantic-ui-react";
 
 export default function RegisterForm(props) {
     const {showLoginForm} = props;
 
     return (
-        <div>
-            <h2>Estamos en el formulario de Registro</h2>
-            <button onClick={showLoginForm}>Ir al login</button>
-        </div>
+        <Form className="login-form">
+            <Form.Input name="name" type="text" placeholder="Nombre" />
+            <Form.Input name="lastname" type="text" placeholder="Apellido" />
+            <Form.Input name="username" type="text" placeholder="Nombre de usuario" />
+            <Form.Input name="email" type="text" placeholder="Correo electronico" />
+            <Form.Input name="password" type="password" placeholder="Contraseña" />
+            <div className="actions">
+                <Button type="button" basic>
+                    Iniciar sesión
+                </Button>
+                <Button type="submit" className="submit">
+                    Registrar
+                </Button>
+            </div>
+        </Form>
     )
 }
