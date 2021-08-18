cf18cc208a9d87ac3e99839efca1958f605a87b3
speraignacio <speraignacio@gmail.com>
2021-08-18 09:39:54 +0200

    create pag user

--- components/Header/Menu/Menu.js
+++ components/Header/Menu/Menu.js
@@ -2,61 +2,67 @@ import { useState } from "react";
 import { Container, Menu, Grid, Icon, Label } from "semantic-ui-react";
 import Link from "next/link";
 import BasicModal from "../../Modal/BasicModal";
-import Auth from "../../Auth"
+import Auth from "../../Auth";
+import useAuth from "../../../hooks/useAuth";
 
 export default function MenuWeb() {
-    
-    const [showModal, setShowModal] = useState(false);
-    const [titleModal, setTitleModal] = useState("Inicia sesión")
+  const [showModal, setShowModal] = useState(false);
+  const [titleModal, setTitleModal] = useState("Inicia sesión");
+  const { logout } = useAuth();
 
+  const onShowModal = () => setShowModal(true);
+  const onCloseModal = () => setShowModal(false);
 
-    const onShowModal = () => setShowModal(true);
-    const onCloseModal = () => setShowModal(false);
-
-    return (
-        <div className="menu">
-            <Container>
-                <Grid>
-                    <Grid.Column className="menu__left" width={6}>
-                        <MenuCategories />
-                    </Grid.Column>
-                    <Grid.Column className="menu__right" width={10}>
-                        <MenuOptions onShowModal={onShowModal} />
-                    </Grid.Column>
-                </Grid>
-            </Container>
-            <BasicModal show={showModal} setShow={setShowModal} title={titleModal} size="small">
-                <Auth onCloseModal={onCloseModal} setTitleModal={setTitleModal} />
-            </BasicModal>
-        </div>
-    );
+  return (
+    <div className="menu">
+      <Container>
+        <Grid>
+          <Grid.Column className="menu__left" width={6}>
+            <MenuCategories />
+          </Grid.Column>
+          <Grid.Column className="menu__right" width={10}>
+            <MenuOptions onShowModal={onShowModal} />
+            <button onClick={logout}>cerrar sesion</button>
+          </Grid.Column>
+        </Grid>
+      </Container>
+      <BasicModal
+        show={showModal}
+        setShow={setShowModal}
+        title={titleModal}
+        size="small"
+      >
+        <Auth onCloseModal={onCloseModal} setTitleModal={setTitleModal} />
+      </BasicModal>
+    </div>
+  );
 }
 
 function MenuCategories() {
-    return (
-        <Menu>
-            <Link href="/indumentaria">
-            <Menu.Item as="a">INDUMENTARIA</Menu.Item>
-            </Link>
-            <Link href="/bumper-plates">
-            <Menu.Item as="a">BUMPER PLATES</Menu.Item>
-            </Link>
-            <Link href="/barras">
-            <Menu.Item as="a">BARRAS</Menu.Item>
-            </Link>
-        </Menu>
-    );
+  return (
+    <Menu>
+      <Link href="/indumentaria">
+        <Menu.Item as="a">INDUMENTARIA</Menu.Item>
+      </Link>
+      <Link href="/bumper-plates">
+        <Menu.Item as="a">BUMPER PLATES</Menu.Item>
+      </Link>
+      <Link href="/barras">
+        <Menu.Item as="a">BARRAS</Menu.Item>
+      </Link>
+    </Menu>
+  );
 }
 
 function MenuOptions(props) {
-    const { onShowModal } = props;
+  const { onShowModal } = props;
 
-    return (
-        <Menu>
-        <Menu.Item onClick={onShowModal}>
-            <Icon name="user outline" />
-            Mi cuenta
-        </Menu.Item>
-        </Menu>
-    );
+  return (
+    <Menu>
+      <Menu.Item onClick={onShowModal}>
+        <Icon name="user outline" />
+        Mi cuenta
+      </Menu.Item>
+    </Menu>
+  );
 }
